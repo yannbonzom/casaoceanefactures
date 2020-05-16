a836bbbf6d1f45cd14ea8218a45c2e999047fc92
Yann Bonzom <yann.bonzom11@gmail.com>
2020-05-16 12:33:15 +0000

    Upload all files

---
+++ app.py
@@ -0,0 +1,241 @@
+from flask import Flask, render_template, request, redirect, send_file, Response, flash, session, flash
+import flask_login
+from werkzeug.security import check_password_hash
+from receipts import clientReceipt, seminarReceipt, mad
+from datetime import date, datetime
+from os import remove
+import sqlite3
+
+
+# Configure app
+app = Flask(__name__)
+app.secret_key = "b'mo&I:/xfc2xfb.x0bc,xe3?xdbJ/xac!$5'"
+
+
+# LOGIN STUFF
+# To set it all up
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+class User(flask_login.UserMixin):
+    pass
+@login_manager.user_loader
+def user_loader(user_id):
+    user = User()
+    user.id = user_id
+    return user
+# Login Page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Display login page
+    if request.method == "GET":
+        return render_template("login.html", firstTime = True)
+
+    # Log user in
+    else:
+        # Using hash of the password Mom decided on
+        if check_password_hash('pbkdf2:sha256:150000$NdXimIou$e43134704cb1264e6532218161e1d8f9f07f4eaea96c0c0807aa7175c3b5c540', request.form['password']):
+            user = User()
+            user.id = 'CASAOCEANE'
+            flask_login.login_user(user)
+            return redirect('/')
+        else:
+            return render_template("login.html", wrongPassword = True)
+# Log out button
+@app.route("/logout")
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    return redirect('/login')
+
+
+# RECEIPT PAGE
+@app.route("/")
+@flask_login.login_required
+def receipt():
+    # Delete any receipt in web app, if any
+    if session['docName']:
+        remove(session['docName'])
+        session['docName'] = None
+
+    return render_template('receipt.html')
+# Client receipt page
+@app.route("/getClientReceipt", methods=["POST"])
+def getClientReceipt():
+    if request.method == "POST":
+        # Process given information to produce the receipt, download it to computer, and store info in db
+
+        # GET ALL USER DATA
+        # Get client source
+        source = request.form["source"]
+        # Get full name
+        firstName = request.form["firstName"].capitalize()
+        lastName = request.form["lastName"].upper()
+        # Get sex
+        gender = request.form["gender"]
+        if gender == "m":
+            sex = "M."
+        else: 
+            sex = "Mme."
+        # Get receipt ID
+        receiptID = int(request.form["ID"])
+        # Set up start date
+        startDateEntry = request.form["startDate"]
+        sYear, sMonth, sDay = map(int, startDateEntry.split("-"))
+        startDate = date(sYear, sMonth, sDay)
+        # Set up end date
+        endDateEntry = request.form["endDate"]
+        eYear, eMonth, eDay = map(int, endDateEntry.split("-"))
+        endDate = date(eYear, eMonth, eDay)
+        # Calculate length of stay from given dates
+        lengthOfStay = (endDate - startDate).days
+        # Get total price
+        totalPrice = float(request.form["totalPrice"])
+
+        # GENERATE FILE AND PROCEED TO DOWNLOAD PAGE
+        # Call the clientReceipt() function to generate it as well as save info in db
+        session['docName'] = clientReceipt(source, firstName, lastName, sex, startDate, endDate, lengthOfStay, receiptID, totalPrice)
+        # Redirect to download page
+        return render_template("download.html")
+# Seminar receipt page
+@app.route("/getSeminarReceipt", methods=["POST"])
+def getSeminarReceipt():
+    if request.method == "POST":
+        # Process given information to produce the receipt, download it to computer, and store info in db
+        
+        # GET ALL USER DATA
+        # Get company name
+        company = request.form["company"].upper()
+        # Get receipt ID
+        receiptID = int(request.form["ID"])
+        # Set up start date
+        startDateEntry = request.form["startDate"]
+        sYear, sMonth, sDay = map(int, startDateEntry.split("-"))
+        startDate = date(sYear, sMonth, sDay)
+        # Set up end date
+        endDateEntry = request.form["endDate"]
+        eYear, eMonth, eDay = map(int, endDateEntry.split("-"))
+        endDate = date(eYear, eMonth, eDay)
+        # Calculate length of stay from given dates
+        lengthOfStay = (endDate - startDate).days + 1
+        # Get total price
+        pricePerDayWithoutTax = float(request.form["pricePerDay"])
+
+        # GENERATE FILE AND PROCEED TO DOWNLOAD PAGE
+        # Call the seminarReceipt() function to generate it as well as save info in db
+        session['docName'] = seminarReceipt(company, receiptID, startDate, endDate, lengthOfStay, pricePerDayWithoutTax)
+        # Redirect to download page
+        return render_template("download.html")
+# To download the receipt when button is clicked
+@app.route("/getReceipt")
+def getReceipt():
+    return send_file(session['docName'], as_attachment = True)
+
+
+# STATISTICS PAGE
+@app.route("/stats", methods=["GET", "POST"])
+@flask_login.login_required
+def stats():
+    # Display all statistics indicators, organized by year
+    if request.method == "GET":
+        # Delete any receipt in web app, if any
+        if session['docName']:
+            remove(session['docName'])
+            session['docName'] = None
+
+        # Current year to limit all db data for Overview Stats to the current year
+        year = str(datetime.now().year)
+        
+        # Open db
+        connection = sqlite3.connect('receipts.db')
+        
+        # REVENUE TABLE
+        # Set up dict with revenue data
+        revenues = {}
+        # Get total revenue from clients
+        clientRevCursor = connection.cursor()
+        clientRevCursor.execute(f"SELECT SUM (totalPrice) FROM receipts WHERE type = 'client' AND endDate >= '{year}-01-01'")
+        clientRevData = clientRevCursor.fetchall()
+        clientRev = float(clientRevData[0][0])
+        revenues['clientRev'] = str(round(clientRev)) + " MAD"
+        # Get total revenue from seminars
+        seminarRevCursor = connection.cursor()
+        seminarRevCursor.execute(f"SELECT SUM (lengthOfStay), AVG (pricePerDayWithoutTax) FROM receipts WHERE type = 'seminar' AND endDate >= '{year}-01-01'")
+        seminarRevData = seminarRevCursor.fetchall()
+        seminarRev = (float(seminarRevData[0][0]) * float(seminarRevData[0][1])) * 1.2
+        revenues['seminarRev'] = str(round(seminarRev)) + " MAD"
+        # Get total revenue (client + seminar)
+        total = clientRev + seminarRev
+        revenues['total'] = str(round(total)) + " MAD"
+        # Get percentages of different revenues
+        revenues['clientPercent'] = round(((clientRev / total) * 100), 1)
+        revenues['seminarPercent'] = round(((seminarRev / total) * 100), 1)
+
+        # CLIENT SOURCES TABLE
+        # Set up dict with source percentages
+        sources = {}
+        # Get total number of clients from AirBnB
+        airbnbCursor = connection.cursor()
+        airbnbCursor.execute(f"SELECT COUNT (*) FROM receipts WHERE type = 'client' AND source = 'AirBnB' AND endDate >= '{year}-01-01'")
+        airbnbData = airbnbCursor.fetchall()
+        airbnbCount = float(airbnbData[0][0])
+        # Get total number of clients from Other
+        otherCursor = connection.cursor()
+        otherCursor.execute(f"SELECT COUNT (*) FROM receipts WHERE type = 'client' AND source = 'Other' AND endDate >= '{year}-01-01'")
+        otherData = otherCursor.fetchall()
+        otherCount = float(otherData[0][0])
+        # Calculate percentages and store in sources dict
+        total = airbnbCount + otherCount
+        sources['percentAirbnb'] = round(((airbnbCount / total) * 100), 1)
+        sources['percentOther'] = round(((otherCount / total) * 100), 1) 
+
+        # ALL CLIENT DATA
+        clientDataCursor = connection.cursor()
+        clientDataCursor.execute("SELECT receiptID, source, firstName, lastName, sex, startDate, endDate, lengthOfStay, totalPrice FROM receipts WHERE type = 'client' ORDER BY endDate DESC, receiptID DESC")
+        clientData = clientDataCursor.fetchall()
+        clientTableLength = len(clientData)
+        # Create Client CSV file to export data
+        clientCSVCursor = connection.cursor()
+        with open('clientCSV.csv', 'w') as file:
+            file.write("Receipt Number,First Name,Last Name,Sex,Start Date,End Date,Length of Stay,Total Price\n")
+            for row in clientCSVCursor.execute("SELECT receiptID, firstName, lastName, sex, startDate, endDate, lengthOfStay, totalPrice FROM receipts WHERE type = 'client' ORDER By endDate DESC, receiptID DESC"):
+                for i in range(len(row)):
+                    file.write(str(row[i]) + ",")
+                file.write('\n')
+
+        # ALL SEMINAR DATA
+        seminarDataCursor = connection.cursor()
+        seminarData = seminarDataCursor.execute("SELECT receiptID, company, startDate, endDate, lengthOfStay, pricePerDayWithoutTax FROM receipts WHERE type = 'seminar' ORDER BY endDate DESC, receiptID DESC")
+        seminarData = seminarDataCursor.fetchall()
+        seminarTableLength = len(seminarData)
+        # Create Seminar CSV file to export data
+        seminarCSVCursor = connection.cursor()
+        with open('seminarCSV.csv', 'w') as file:
+            file.write("Receipt Number, Company, Start Date, End Date, Duration, Price per Day (HT)\n")
+            for row in seminarCSVCursor.execute("SELECT receiptID, company, startDate, endDate, lengthOfStay, pricePerDayWithoutTax FROM receipts WHERE type = 'seminar' ORDER BY endDate DESC, receiptID DESC"):
+                for i in range(len(row)):
+                    file.write(str(row[i]) + ",")
+                file.write('\n')
+
+        # Save and close cursors and db
+        clientDataCursor.close()
+        clientCSVCursor.close()
+        seminarDataCursor.close()
+        seminarCSVCursor.close()
+        clientRevCursor.close()
+        seminarRevCursor.close()
+        airbnbCursor.close()
+        otherCursor.close()
+        connection.commit()
+        connection.close()
+
+        # Send data to the stats page to render it
+        return render_template("stats.html", revenues = revenues, sources = sources, clientData=clientData, clientTableLength=clientTableLength, seminarData=seminarData, seminarTableLength=seminarTableLength)
+# To download the client CSV
+@app.route('/getClientCSV')
+def getClientCSV():
+    return send_file('clientCSV.csv', as_attachment=True)
+# To download the seminar CSV
+@app.route('/getSeminarCSV')
+def getSeminarCSV():
+    return send_file('seminarCSV.csv', as_attachment=True)
---
+++ receipts.py
@@ -0,0 +1,350 @@
+from docx import Document
+import math
+from datetime import date, datetime
+from docx.shared import Pt
+from docx.enum.section import WD_SECTION
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+import sqlite3
+
+
+# Generate client receipt
+def clientReceipt(source, firstName, lastName, sex, startDate, endDate, lengthOfStay, receiptID, totalPrice):
+    # Before proceeding, store all data in the db
+    connection = sqlite3.connect('receipts.db')
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO receipts (type, source, firstName, lastName, sex, startDate, endDate, lengthOfStay, receiptID, totalPrice) VALUES ('client', ?, ?, ?, ?, ?, ?, ?, ?, ?)", (source, firstName, lastName, sex, startDate, endDate, lengthOfStay, receiptID, totalPrice))
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+    # Price calculations
+    withoutTax = round_up(totalPrice / 1.2, 2)
+    tax = round_down(0.2 * withoutTax, 2)
+
+    # Open template which we'll later save in another document
+    document = Document('clientTemplate.docx')
+
+    # Font settings
+    # For all 'paragraphs'
+    font = document.styles['Normal'].font
+    font.name = 'Arial'
+    font.size = Pt(13)
+    font.bold = True
+    # For 'Heading 1'
+    font = document.styles['Heading 1'].font
+    font.name = 'Arial'
+    font.size = Pt(16)
+    font.bold = True
+    # For 'Heading 2'
+    font = document.styles['Heading 2'].font
+    font.name = 'Arial'
+    font.size = Pt(12)
+    font.bold = False
+
+    # Populate document with user-data 
+    # In paragraphs
+    for paragraph in document.paragraphs:
+        if "<firstParagraph>" in paragraph.text:
+            if lengthOfStay == 1:
+                if startDate.year != endDate.year:
+                    paragraph.text = f"Durée séjour de {sex} {lastName}: 1 jour, du {two(startDate.day)}/{two(startDate.month)}/{startDate.year} au {two(endDate.day)}/{two(endDate.month)}/{endDate.year}, au prix de {mad(totalPrice)} TTC."
+                else: 
+                    paragraph.text = f"Durée séjour de {sex} {lastName}: 1 jour, du {two(startDate.day)}/{two(startDate.month)} au {two(endDate.day)}/{two(endDate.month)}/{endDate.year}, au prix de {mad(totalPrice)} TTC."
+            else: 
+                if startDate.year != endDate.year:
+                    paragraph.text = f"Durée séjour de {sex} {lastName}: {lengthOfStay} jours, du {two(startDate.day)}/{two(startDate.month)}/{startDate.year} au {two(endDate.day)}/{two(endDate.month)}/{endDate.year}, au prix de {mad(totalPrice)} TTC."
+                else: 
+                    paragraph.text = f"Durée séjour de {sex} {lastName}: {lengthOfStay} jours, du {two(startDate.day)}/{two(startDate.month)} au {two(endDate.day)}/{two(endDate.month)}/{endDate.year}, au prix de {mad(totalPrice)} TTC."
+        elif "<totalPrice>" in paragraph.text:
+            paragraph.text = f"NET A PAYER EN MAD:  {mad(totalPrice)} TTC"
+        elif "<inWords>" in paragraph.text:
+            paragraph.text = f"{toWords(totalPrice)}, TTC"
+    # In top table
+    document.tables[0].cell(1,0).text = f"N° de FACTURE {receiptID}-{endDate.year}"
+    document.tables[0].cell(1,1).text = f"{sex} {firstName} {lastName}"
+    document.tables[0].cell(2,1).text = f"DATE: {two(endDate.day)}/{two(endDate.month)}/{endDate.year}"
+    # In center table (with left-alignment)
+    number1 = document.tables[1].cell(0,1).paragraphs[0]
+    number1.add_run(f"{mad(withoutTax)}")
+    number2 = document.tables[2].cell(0,1).paragraphs[0]
+    number2.add_run(f"{mad(tax)}")
+    number3 = document.tables[3].cell(0,1).paragraphs[0]
+    number3.add_run(f"{mad(totalPrice)}")
+
+    # Save document with formatted name
+    docName = "FACTURE MDB Maroc Services " + str(receiptID) + "-" + str(endDate.year) + ".docx"
+    document.save(docName)
+    
+    # Download it to the user's computer
+    return docName
+
+# Generate seminar receipt
+def seminarReceipt(company, receiptID, startDate, endDate, lengthOfStay, pricePerDayWithoutTax):
+    # Before proceeding, store all data in the db
+    connection = sqlite3.connect('receipts.db')
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO receipts (type, company, receiptID, startDate, endDate, lengthOfStay, pricePerDayWithoutTax) VALUES ('seminar', ?, ?, ?, ?, ?, ?)", (company, receiptID, startDate, endDate, lengthOfStay, pricePerDayWithoutTax))
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+    # Price calculations
+    withoutTax = float(lengthOfStay) * pricePerDayWithoutTax
+    tax = round_down(0.2 * withoutTax)
+    totalPrice = round_up(withoutTax + tax)
+
+    # Open template which we'll later save in another document
+    document = Document('seminarTemplate.docx')
+
+    # Font settings
+    # For all 'paragraphs'
+    font = document.styles['Normal'].font
+    font.name = 'Arial'
+    font.size = Pt(13)
+    font.bold = True
+    # For 'Heading 1'
+    font = document.styles['Heading 1'].font
+    font.name = 'Arial'
+    font.size = Pt(16)
+    font.bold = True
+    # For 'Heading 2'
+    font = document.styles['Heading 2'].font
+    font.name = 'Arial'
+    font.size = Pt(12)
+    font.bold = False
+
+    # Populate document with user-data 
+    # In paragraphs
+    for paragraph in document.paragraphs:
+        if "<firstParagraph>" in paragraph.text:
+            if lengthOfStay == 1:
+                paragraph.text = f"SEMINAIRE: 1 jour, le {two(startDate.day)}/{two(startDate.month)}/{startDate.year}, au prix de"
+            else: 
+                if startDate.year != endDate.year:
+                    paragraph.text = f"SEMINAIRE: {lengthOfStay} jours, du {two(startDate.day)}/{two(startDate.month)}/{startDate.year} au {two(endDate.day)}/{two(endDate.month)}/{endDate.year}, au prix par jour"
+                else: 
+                    paragraph.text = f"SEMINAIRE: {lengthOfStay} jours, du {two(startDate.day)}/{two(startDate.month)} au {two(endDate.day)}/{two(endDate.month)}/{endDate.year}, au prix par jour"
+        elif "<firstParagraphPricePart>" in paragraph.text:
+            paragraph.text = f"de {mad(pricePerDayWithoutTax)} HT."
+        elif "<totalPrice>" in paragraph.text:
+            paragraph.text = f"NET A PAYER EN MAD:  {mad(totalPrice)} TTC"
+        elif "<inWords>" in paragraph.text:
+            paragraph.text = f"{toWords(totalPrice)}, TTC"
+    # In top table
+    document.tables[0].cell(1,0).text = f"N° de FACTURE {receiptID}-{endDate.year}"
+    document.tables[0].cell(2,1).text = f"DATE: {two(endDate.day)}/{two(endDate.month)}/{endDate.year}"
+    # In center table (with left-alignment)
+    number1 = document.tables[1].cell(0,1).paragraphs[0]
+    number1.add_run(f"{mad(withoutTax)}")
+    number2 = document.tables[2].cell(0,1).paragraphs[0]
+    number2.add_run(f"{mad(tax)}")
+    number3 = document.tables[3].cell(0,1).paragraphs[0]
+    number3.add_run(f"{mad(totalPrice)}")
+
+    # Save document with formatted name
+    docName = "FACTURE MDB Maroc Services " + str(receiptID) + "-" + str(endDate.year) + " " + company + ".docx"
+    document.save(docName)
+    
+    # Download it to the user's computer
+    return docName
+
+
+# FUNCTIONS NECESSARY FOR clientReceipt() and seminarReceipt()
+# Format money values in MAD
+def mad(n):
+    formatted = '{: ,.2f}'.format(n).replace(',', ' ').replace('.', ',')
+    return f"{formatted} MAD"
+# Format dates to two digits (ex. 1 -> 01)
+def two(n):
+    formatted = '{0:02}'.format(n)
+    return formatted
+# Rounding definitions
+def round_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.ceil(n * multiplier) / multiplier
+def round_down(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n * multiplier) / multiplier
+
+    # Set up number dictionaries
+    numbers = {
+        "1": "un",
+        "2": "deux",
+        "3": "trois",
+        "4": "quatre",
+        "5": "cinq",
+        "6": "six",
+        "7": "sept",
+        "8": "huit", 
+        "9": "neuf", 
+        "11": "onze",
+        "12": "douze", 
+        "13": "treize",
+        "14": "quatorze",
+        "15": "quinze",
+        "16": "seize"
+    }
+    tens = {
+        "1": "dix", 
+        "2": "vingt",
+        "3": "trente",
+        "4": "quarante", 
+        "5": "cinquante",
+        "6": "soixante",
+        "7": "soixante-dix",
+        "8": "quatre-vingt",
+        "9": "quatre-vingt-dix",
+    }
+
+    # Make any block of threes actually have three numbers (ex. 9 --> 009)
+    threes = int(threes)
+    threes = "{0:03}".format(threes)
+
+    # Declare the return value toWords
+    toWords = ""
+    
+    # State hundreds place
+    if threes[0] != "0":
+        # Account for spelling difference cent-cents
+        if threes[1] == "0" and threes[2] == "0":
+            toWords = numbers[threes[0]] + " cents"
+            return toWords
+        else:
+            toWords = numbers[threes[0]] + " cent "
+    # Account for numbers from 11-16
+    if f"{threes[1]}{threes[2]}" in numbers.keys():
+        toWords += numbers[f"{threes[1]}{threes[2]}"]
+        return toWords
+    # State tens place
+    if threes[1] != "0":
+        toWords += tens[threes[1]] + " "
+    # State ones place
+    if threes[2] != "0":
+        toWords += numbers[threes[2]]
+
+    # Return the written out form
+    return toWords
+# Converting price in numbers into words
+def toWords(n):
+    # inWords string
+    inWords = ""
+
+    # Convert number into formatted string for easy manipulation
+    n = f"{n: ,.2f}"
+
+    # Split n into whole and decimal parts based on period
+    nSplitted = n.split(".")
+    whole = nSplitted[0]
+    decimal = nSplitted[1]
+
+    # Split whole part into blocks of three numbers based on commas
+    threes = whole.split(",")
+
+    # Iterate over each block of threes to write it in words
+    # If in the thousands
+    if len(threes) == 2:
+        inWords = read(threes[0]) + " mille " + read(threes[1]) + " dirhams"
+    else:
+        inWords = read(threes[0]) + " dirhams"
+
+    # Read the cents
+    if decimal == "01":
+        inWords += " et un centime"
+    elif decimal != "00":
+        inWords += " et " + read(decimal) + " centimes"
+    
+    # Remove 'un' if it appears at the string's beginning
+    unFix = inWords.split()
+    if unFix[0] == "un":
+        i = 1
+        inWords = ""
+        while i < len(unFix):
+            # if/else accounts for space added to inWords's very end
+            if i != len(unFix) - 1:
+                inWords += unFix[i] + " "
+            else:
+                inWords += unFix[i]
+            i += 1
+    
+    # Return capitalized string
+    return inWords.capitalize()
+# Part of the toWords function
+def read(threes):
+    # Set up number dictionaries
+    numbers = {
+        "1": "un",
+        "2": "deux",
+        "3": "trois",
+        "4": "quatre",
+        "5": "cinq",
+        "6": "six",
+        "7": "sept",
+        "8": "huit", 
+        "9": "neuf", 
+        "11": "onze",
+        "12": "douze", 
+        "13": "treize",
+        "14": "quatorze",
+        "15": "quinze",
+        "16": "seize",
+        "71": "soixante et onze",
+        "72": "soixante-douze",
+        "73": "soixante-treize",
+        "74": "soixante-quatorze",
+        "75": "soixante-quinze",
+        "76": "soixante-seize",
+        "91": "quatre-vingt-onze",
+        "92": "quatre-vingt-douze",
+        "93": "quatre-vingt-treize",
+        "94": "quatre-vingt-quatorze",
+        "95": "quatre-vingt-quinze",
+        "96": "quatre-vingt-seize",
+    }
+    tens = {
+        "1": "dix", 
+        "2": "vingt",
+        "3": "trente",
+        "4": "quarante", 
+        "5": "cinquante",
+        "6": "soixante",
+        "7": "soixante-dix",
+        "8": "quatre-vingt",
+        "9": "quatre-vingt-dix",
+    }
+
+    # Make any block of threes actually have three numbers (ex. 9 --> 009)
+    threes = int(threes)
+    threes = "{0:03}".format(threes)
+
+    # Declare the return value toWords
+    toWords = ""
+    
+    # State hundreds place
+    if threes[0] != "0":
+        # Account for spelling difference cent-cents
+        if threes[1] == "0" and threes[2] == "0":
+            toWords = numbers[threes[0]] + " cents"
+            return toWords
+        else:
+            toWords = numbers[threes[0]] + " cent "
+    # Account for numbers from 11-16
+    if f"{threes[1]}{threes[2]}" in numbers.keys():
+        toWords += numbers[f"{threes[1]}{threes[2]}"]
+        return toWords
+    # State tens place
+    if threes[1] != "0":
+        # Account for '-' connection when in 70s or 90s
+        if threes[1] == "7" or threes[1] == "9":
+            toWords += tens[threes[1]]
+        else: 
+            toWords += tens[threes[1]] + " "
+    # State ones place
+    if threes[2] != "0":
+        # Account for '-' connection when in 70s or 90s
+        if threes[1] == "7" or threes[1] == "9":
+            toWords += "-" + numbers[threes[2]]
+        else: 
+            toWords += numbers[threes[2]]
+
+    # Return the written out form
+    return toWords
